2e8f2835dc967973087cff24e61760ded726ebb6
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-04-05 10:14:56 +0200

    MudBlazor SnackBar implemented.

--- EmpAppBlazor/Client/Services/AuthService/AuthService.cs
+++ EmpAppBlazor/Client/Services/AuthService/AuthService.cs
@@ -1,19 +1,30 @@
 ﻿using EmpAppBlazor.Shared.Auth;
+using MudBlazor;
 
 namespace EmpAppBlazor.Client.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         private readonly HttpClient _http;
+        private readonly ISnackbar _snackBar;
 
-        public AuthService(HttpClient http)
+        public AuthService(HttpClient http, ISnackbar snackBar)
         {
             _http = http;
+            _snackBar = snackBar;
         }
 
         public async Task<ServiceResponse<int>> Register(UserRegister request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
+            if(result.IsSuccessStatusCode)
+            {
+                _snackBar.Add("New User Registered", Severity.Success); 
+            }
+            else
+            {
+                _snackBar.Add("Registration failure", Severity.Error);
+            }
             return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
         }
     }
