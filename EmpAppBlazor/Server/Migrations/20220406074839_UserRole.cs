c0e9bad0d13f96ade7af065b6675f7bdb5f255cb
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-04-06 10:44:14 +0200

    Role based authorization implemented.

---
+++ EmpAppBlazor/Server/Migrations/20220406074839_UserRole.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmpAppBlazor.Server.Migrations
+{
+    public partial class UserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 4, 11, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6511));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 5, 11, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6515));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 4, 10, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6518));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DueDate",
+                value: new DateTime(2022, 7, 15, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6348));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DueDate",
+                value: new DateTime(2022, 9, 3, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6388));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DueDate",
+                value: new DateTime(2022, 10, 23, 9, 48, 39, 86, DateTimeKind.Local).AddTicks(6391));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 4, 9, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(9079));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 5, 9, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(9090));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DeliveryDate",
+                value: new DateTime(2022, 4, 8, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(9098));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DueDate",
+                value: new DateTime(2022, 7, 13, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(8804));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DueDate",
+                value: new DateTime(2022, 9, 1, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(8859));
+
+            migrationBuilder.UpdateData(
+                table: "Workloads",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DueDate",
+                value: new DateTime(2022, 10, 21, 13, 27, 33, 442, DateTimeKind.Local).AddTicks(8866));
+        }
+    }
+}
--- EmpAppBlazor/Server/Services/AuthService/AuthService.cs
+++ EmpAppBlazor/Server/Services/AuthService/AuthService.cs
@@ -43,7 +43,8 @@ namespace EmpAppBlazor.Server.Services.AuthService
             List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
