ab1353f20913b5a6df299951c61878cd386d719d
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-04-12 14:27:49 +0200

    Started work on Tasks System

---
+++ EmpAppBlazor/Shared/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿namespace EmpAppBlazor.Shared
+{
+    public class TaskItem
+    {
+        public int TaskItemId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int AssignedToId { get; set; }
+        public int AuthorId { get; set; }
+        public int? EditorId { get; set; }
+        public int? ProjectId { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string TaskStatus { get; set; } = string.Empty;
+        public string Importance { get; set; } = string.Empty;
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateEdited { get; set; }
+    }
+}
