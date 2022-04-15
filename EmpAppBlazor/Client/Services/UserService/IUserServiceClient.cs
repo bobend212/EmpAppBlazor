bf817b9d38ddfdf70c3df31f6debba4023ec7d8b
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-04-15 15:45:40 +0200

    Started implementing UserService on Client side

---
+++ EmpAppBlazor/Client/Services/UserService/IUserServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using EmpAppBlazor.Shared.Auth;
+
+namespace EmpAppBlazor.Client.Services.UserService
+{
+    public interface IUserServiceClient
+    {
+        List<User> Users { get; set; }
+
+        Task GetAllUsers();
+    }
+}
---
+++ EmpAppBlazor/Client/Services/UserService/UserServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using EmpAppBlazor.Shared.Auth;
+using MudBlazor;
+
+namespace EmpAppBlazor.Client.Services.UserService
+{
+    public class UserServiceClient : IUserServiceClient
+    {
+        private readonly HttpClient _http;
+        private readonly ISnackbar _snackBar;
+
+        public UserServiceClient(HttpClient http, ISnackbar snackBar)
+        {
+            _http = http;
+            _snackBar = snackBar;
+        }
+
+        public List<User> Users { get; set; } = new List<User>();
+
+        public async Task GetAllUsers()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<User>>>("/api/User");
+            if (result != null && result.Data != null)
+                Users = result.Data;
+        }
+    }
+}
